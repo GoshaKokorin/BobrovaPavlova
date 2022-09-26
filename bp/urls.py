091da81fd7522d825a48d6df0b639771baf82bd4
Gosha Kokorin <go_2002@mail.ru>
2022-09-26 20:20:00 +0500

    add templates

--- bp/urls.py
+++ bp/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from apps.index.urls import urlpatterns as index_urlpatterns
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.index.urls'), name='index'),
 ]
-urlpatterns += index_urlpatterns
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
