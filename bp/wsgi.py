8d5a9f73a1e368bb2007d352d1fb00b8c76d7ff3
Gosha Kokorin <go_2002@mail.ru>
2022-09-26 20:55:49 +0500

    change gitignore

--- bp/wsgi.py
+++ bp/wsgi.py
@@ -1,9 +1,3 @@
-import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-sys.path.insert(0, '/var/www/u0000006/data/www/faq-reg.ru/project_name')
-sys.path.insert(1, '/var/www/u0000006/data/djangoenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'bp.settings'
 
 application = get_wsgi_application()
