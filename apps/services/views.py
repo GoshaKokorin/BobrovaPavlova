b8a468947cf0789a0b8847442a8c71f1cee88c7c
Gosha Kokorin <go_2002@mail.ru>
2022-12-20 21:59:42 +0500

    change services page

--- apps/services/views.py
+++ apps/services/views.py
@@ -19,6 +19,21 @@ class ServicesView(TemplateView):
         context['residential'] = get_object_or_404(Services, pk=3)
         context['commercial'] = get_object_or_404(Services, pk=4)
 
+        context['architecture_projects'] = Projects.objects.filter(services_project_id=2, main=True, publish=True)
+        context['architecture_project1'] = context['architecture_projects'][0]
+        context['architecture_project2'] = context['architecture_projects'][1]
+        context['architecture_project3'] = context['architecture_projects'][2]
+
+        context['residential_projects'] = Projects.objects.filter(services_project_id=3, main=True, publish=True)
+        context['residential_project1'] = context['residential_projects'][0]
+        context['residential_project2'] = context['residential_projects'][1]
+        context['residential_project3'] = context['residential_projects'][2]
+
+        context['commercial_projects'] = Projects.objects.filter(services_project_id=4, main=True, publish=True)
+        context['commercial_project1'] = context['commercial_projects'][0]
+        context['commercial_project2'] = context['commercial_projects'][1]
+        context['commercial_project3'] = context['commercial_projects'][2]
+
         context['main_blog'] = get_object_or_404(Blog, main=True)
         context['other_blog'] = Blog.objects.filter(publish=True).order_by('-pk')[:2]
 
