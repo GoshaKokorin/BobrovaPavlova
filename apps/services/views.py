97ac66b8b0869cc606ad5bbd493ae099dc1f0901
Gosha Kokorin <go_2002@mail.ru>
2023-07-06 15:19:02 +0500

    delete: commerc context

--- apps/services/views.py
+++ apps/services/views.py
@@ -17,7 +17,6 @@ class ServicesView(TemplateView):
 
         context['architecture'] = get_object_or_404(Services, pk=2)
         context['residential'] = get_object_or_404(Services, pk=3)
-        context['commercial'] = get_object_or_404(Services, pk=4)
 
         context['architecture_projects'] = Projects.objects.filter(services_project_id=2, main=True, publish=True)
         context['architecture_project1'] = context['architecture_projects'][0]
@@ -28,11 +27,6 @@ class ServicesView(TemplateView):
         context['residential_project1'] = context['residential_projects'][0]
         context['residential_project2'] = context['residential_projects'][1]
         context['residential_project3'] = context['residential_projects'][2]
-
-        context['commercial_projects'] = Projects.objects.filter(services_project_id=4, main=True, publish=True)
-        context['commercial_project1'] = context['commercial_projects'][0]
-        context['commercial_project2'] = context['commercial_projects'][1]
-        context['commercial_project3'] = context['commercial_projects'][2]
 
         context['main_blog'] = get_object_or_404(Blog, main=True)
         context['other_blog'] = Blog.objects.filter(publish=True).order_by('-pk')[:2]
