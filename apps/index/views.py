374ed7fec109ca80faf36a16368870b50bf5d6c5
Valentin <alex.filolog@mail.ru>
2023-07-06 13:34:18 +0300

    “correction”

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView
-from apps.services.models import Services
-from apps.services.models import Projects
+
 from apps.blog.models import Blog
+from apps.services.models import Projects
+from apps.services.models import Services
 
 
 def index(request):
@@ -10,7 +11,6 @@ def index(request):
 
     context['architecture'] = get_object_or_404(Services, pk=2)
     context['residential'] = get_object_or_404(Services, pk=3)
-    context['commercial'] = get_object_or_404(Services, pk=4)
 
     context['architecture_projects'] = Projects.objects.filter(services_project_id=2, main=True, publish=True)
     context['architecture_project1'] = context['architecture_projects'][0]
@@ -22,26 +22,18 @@ def index(request):
     context['residential_project2'] = context['residential_projects'][1]
     context['residential_project3'] = context['residential_projects'][2]
 
-    context['commercial_projects'] = Projects.objects.filter(services_project_id=4, main=True, publish=True)
-    context['commercial_project1'] = context['commercial_projects'][0]
-    context['commercial_project2'] = context['commercial_projects'][1]
-    context['commercial_project3'] = context['commercial_projects'][2]
-
     context['main_blog'] = get_object_or_404(Blog, main=True)
     context['other_blog'] = Blog.objects.filter(publish=True).order_by('-pk')[:2]
-
     return render(request, 'index.html', context)
 
 
 def contact(request):
     context = {}
-
     return render(request, 'contacts.html', context)
 
 
 def partners(request):
     context = {}
-
     return render(request, 'partners.html', context)
 
 
